9062c3ac0e9f994131ee14badb172d195d12b228
programmerarantes <programmerarantes@gmail.com>
2024-10-18 10:33:19 -0300

    feat: finishing the pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Hero />
       <Features/>
+      <Pricing/>
     </main>
   )
 }
