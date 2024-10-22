553ef4eff5756801fc305877ab013be13db55214
programmerarantes <programmerarantes@gmail.com>
2024-10-22 15:17:55 -0300

    feat: creating footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Faq from "./components/Faq";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
@@ -14,6 +15,7 @@ export default function App() {
       <Pricing/>
       <Faq />
       <Testimonials />
+      <Footer />
     </main>
   )
 }
